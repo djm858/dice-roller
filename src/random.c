cd39c96debaf79ee0b2b86651a91383c4c0130fe
djm858 <duncanjmcfarland@gmail.com>
2026-03-10 12:48:28 -0500

    improve random_seed_generate for c89

--- src/random.c
+++ src/random.c
@@ -2,19 +2,14 @@
 
 /*
  * When a program a function such as rand() to obtain random values, it is
- * necessary to generate a random seed near the beginning of the main function.
+ * necessary to generate a random seed at the beginning of the main function.
  * If not, the program will roll the exact same numbers each time the program
- * runs. This method below generates a seed based on the current nanosecond of
- * the host computer. It also factors in the process ID of the program running
+ * runs. This method below generates a seed based on the current second of the
+ * host computer. It also factors in the process ID of the program running
  * (via getpid) so that if two or more of these programs were initiating at
  * exactly the same time they would still generate different random seeds.
  */
 void random_seed_generate()
 {
-	/*
-	 * struct timespec ts;
-	 * clock_gettime(CLOCK_MONOTONIC, &ts);
-	 * srand(ts.tv_nsec ^ getpid());
-	 */
-	srand(time(NULL));
+	srand(time(NULL) + getpid());
 }
